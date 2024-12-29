3f4e016299fd4b878b54e5bf779d4cd00fe13c80
wisberry2022 <wisberry2022@gmail.com>
2024-12-29 14:43:50 +0900

    rating

--- src/5_entities/question/ui/RatingQuestion.tsx
+++ src/5_entities/question/ui/RatingQuestion.tsx
@@ -1,6 +1,7 @@
 import { RatingQuestion as RatingQuestionResponse } from "@/6_shared";
 import { FC } from "react";
 import styles from "./styles/rating-question.module.css";
+import { FaRegStar } from "react-icons/fa";
 
 type RatingQuestionProps = {
   question: RatingQuestionResponse;
@@ -11,6 +12,14 @@ export const RatingQuestion: FC<RatingQuestionProps> = (props) => {
   return (
     <div className={styles.qContainer}>
       <h4>{question.question}</h4>
+      <div className={styles.starts}>
+        {Array.from({ length: question.ratingLimit / question.score }).map(
+          (_, i) => (
+            <FaRegStar key={i} />
+          )
+        )}
+      </div>
+      <div className={styles.scoreBox}>( 0 / {question.ratingLimit}점 )</div>
     </div>
   );
 };
