82de3b31b8c204ba87a90a5dc0149d076d578925
wisberry2022 <wisberry2022@gmail.com>
2025-02-09 08:36:39 +0900

    lng

--- src/6_shared/ui/lnb/LeftLNB.tsx
+++ src/6_shared/ui/lnb/LeftLNB.tsx
@@ -2,19 +2,36 @@ import { FC } from "react";
 import styles from "./styles/left-lnb.module.css";
 import { MdOutlineCreate, MdOutlineDocumentScanner } from "react-icons/md";
 import { FaPodcast } from "react-icons/fa";
+import { useRouter } from "next/router";
+import { endpoints } from "@/6_shared/constants";
 
 export const LeftLNB: FC = () => {
+  const router = useRouter();
+
+  const goPage = (link: string) => {
+    router.push(link);
+  };
+
   return (
     <div className={styles.lnb}>
-      <div className={styles.iconBtn}>
+      <div
+        className={styles.iconBtn}
+        onClick={() => goPage(endpoints.form.index)}
+      >
         <MdOutlineDocumentScanner />
         <span>양식 관리</span>
       </div>
-      <div className={styles.iconBtn}>
+      <div
+        className={styles.iconBtn}
+        onClick={() => goPage(endpoints.form.create)}
+      >
         <MdOutlineCreate />
         <span>양식 생성</span>
       </div>
-      <div className={styles.iconBtn}>
+      <div
+        className={styles.iconBtn}
+        onClick={() => goPage(endpoints.survey.index)}
+      >
         <FaPodcast />
         <span>설문조사 관리</span>
       </div>
