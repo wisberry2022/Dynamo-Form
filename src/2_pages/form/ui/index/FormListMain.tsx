2db53d05b06f456fe55187843b5b0b3ef277e865
wisberry2022 <wisberry2022@gmail.com>
2025-02-09 09:07:53 +0900

    list button css fixed

--- src/2_pages/form/ui/index/FormListMain.tsx
+++ src/2_pages/form/ui/index/FormListMain.tsx
@@ -2,12 +2,23 @@ import { FC } from "react";
 import styles from "./styles/form-list.module.css";
 import { useFormListSWR } from "@/6_shared/api";
 import { FormList } from "@/3_widgets/form";
+import { Button, endpoints } from "@/6_shared";
+import { useRouter } from "next/router";
 
 export const FormListMain: FC = () => {
   const { formList } = useFormListSWR();
+  const router = useRouter();
+
+  const goCreate = () => {
+    router.push(endpoints.form.create);
+  }
 
   return (
     <div id={styles.listContainer}>
+      <div className={styles.listHeader}>
+        <h3>양식 목록</h3>
+        <Button onClick={goCreate}>양식 추가</Button>
+      </div>
       <FormList formList={formList?.data} />
     </div>
   );
